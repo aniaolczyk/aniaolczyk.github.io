92db146a6fec0375200922388d59e46e69ad0e0d
Dominik Olczyk <dolczyk@rippling.com>
2022-09-17 20:18:29 +0200

    add react

---
+++ src/app.js
@@ -0,0 +1,109 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import "./style.css"
+
+import Nav from "./nav";
+
+
+export default function App() {
+  return (
+    <Router>
+      <Nav />
+    <div className="divider"></div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/music">
+        <Music />
+      </Route>
+      <Route path="/concerts">
+        <Concerts />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
+    </Switch>
+  </Router>
+  );
+}
+
+function About() {
+  return (
+    <section className="section">
+      <div className="container is-fluid">
+        <h1 className="title header-ania text-black">
+          O mnie
+        </h1>
+        <p className="subtitle text-black">
+          Coming soon...
+        </p>
+      </div>
+    </section>
+  );
+}
+
+function Home() {
+  return (<About />);
+}
+
+function Music() {
+  return (
+    <section className="section">
+      <div className="container is-fluid">
+        <h1 className="title header-ania text-black">
+          Muzyka
+        </h1>
+        <p className="subtitle text-black">
+          Spotify
+        </p>
+        <p className="subtitle text-black">
+          Apple Music
+        </p>
+        <p className="subtitle text-black">
+          YouTube
+        </p>
+      </div>
+    </section>
+  );
+}
+
+function Concerts() {
+  return (
+    <section className="section">
+      <div className="container is-fluid">
+        <h1 className="title header-ania text-black">
+          Koncerty
+        </h1>
+        <p className="subtitle text-black">
+          16 Października - Stakkato Art - Kraków
+        </p>
+      </div>
+    </section>
+  );
+}
+
+function Contact() {
+  return (
+    <section className="section">
+      <div className="container is-fluid">
+        <h1 className="title header-ania text-black">
+          Kontakt
+        </h1>
+        <p className="subtitle text-black">
+          Booking - booking@aniaolczyk.com<br />
+          Reszta spraw - info@aniaolczyk.com
+        </p>
+      </div>
+    </section>
+  );
+}
---
+++ src/nav.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  useLocation,
+} from "react-router-dom";
+import "./style.css"
+
+export default function Nav() {
+  const location = useLocation();
+  const [mobileExpanded, setMobileExpanded] = useState(false);
+  console.log(location.pathname);
+  return (
+    <div>
+      <div>
+        <h1 className="logo-header">
+          ANIA OLCZYK
+        </h1>
+      </div>
+      <div className="navbar-brand">
+        <a role="button" className={`navbar-burger ${mobileExpanded ? "is-active" : ""}`} onClick={() => setMobileExpanded(!mobileExpanded)}aria-label="menu" aria-expanded="true">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+        {/* <div role="button" className={`navbar-burger ${mobileExpanded ? "is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"><NavLink activeClassName="navbar-in-active" className="navbar-in" to="/about">o mnie</NavLink></span>
+          <span aria-hidden="true"><NavLink activeClassName="navbar-in-active" className="navbar-in" to="/music">muzyka</NavLink></span>
+          <span aria-hidden="true"><NavLink activeClassName="navbar-in-active" className="navbar-in" to="/concerts">koncerty</NavLink></span>
+          <span aria-hidden="true"><NavLink activeClassName="navbar-in-active" className="navbar-in" to="/contact">kontakt</NavLink></span>
+        </div> */}
+      </div>
+      <nav className={`navbar navbar-ania ${mobileExpanded ? "is-active" : ""}`} role="navigation" aria-label="main navigation">
+        <div id="navbar" className={`navbar-menu ${mobileExpanded ? "is-active" : ""}`}>
+            <div className="navbar-start navbar-center">
+              <div className="navbar-item navbar-ania-item">
+                <NavLink activeClassName="navbar-in-active" onClick={() => setMobileExpanded(false)} className="navbar-in navbar-ania-item" to="/about">o mnie</NavLink>
+              </div>
+          
+              <div className="navbar-item navbar-ania-item">
+                <NavLink activeClassName="navbar-in-active" onClick={() => setMobileExpanded(false)} className="navbar-in" to="/music">muzyka</NavLink>
+              </div>
+          
+              <div className="navbar-item navbar-ania-item">
+                <NavLink activeClassName="navbar-in-active" onClick={() => setMobileExpanded(false)} className="navbar-in" to="/concerts">koncerty</NavLink>
+              </div>
+          
+              <div className="navbar-item navbar-ania-item">
+                <NavLink activeClassName="navbar-in-active" onClick={() => setMobileExpanded(false)} className="navbar-in" to="/contact">kontakt</NavLink>
+              </div>
+            </div>
+          </div>
+      </nav>
+    </div>
+  )
+}
