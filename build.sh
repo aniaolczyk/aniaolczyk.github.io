32c4ad90378eb3d4d8de1ba273bd1146c209548a
Dominik Olczyk <dolczyk@rippling.com>
2022-11-27 14:03:02 +0700

    fix

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 rm -r ../build/
 npm run build
 gh-pages -d build
-cp -r build/ ../build
 /opt/homebrew/bin/python3 add_google_script.py
+cp -r build/ ../build
 echo "Successfully built website."
 echo "execute:
 gc master
