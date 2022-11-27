dd65fc0f8b288d79b8cc1d70537fd67177bd0094
Dominik Olczyk <dolczyk@rippling.com>
2022-11-27 14:08:22 +0700

    new script

--- build.sh
+++ build.sh
@@ -4,6 +4,14 @@ npm run build
 gh-pages -d build
 /opt/homebrew/bin/python3 add_google_script.py
 cp -r build/ ../build
+git add .
+git commit -m "$1"
+git push
+gc master
+cp -r ../build .
+git add .
+git commit -m "$1"
+git push
 echo "Successfully built website."
 echo "execute:
 gc master
