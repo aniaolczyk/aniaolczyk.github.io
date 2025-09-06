3657c8bc8f1ae263ad2d67609d9774b7dd3b1a6c
Dominik Olczyk <dominik.olczyk@gmail.com>
2025-09-06 21:48:55 +0200

    asd

--- src/components/QRPage.js
+++ src/components/QRPage.js
@@ -7,8 +7,14 @@ const QRPage = () => {
       <div className="container qr-container">
         <div className="qr-layout">
           <div className="qr-info">
-            <h2>Heading</h2>
-            <p>Jestem Ania Olczyk, bla bla bla...</p>
+            <h2>Cześć, </h2>
+            <p>
+Tu Ania Olczyk, Piszę piosenki, które słuchają Ciebie.
+Moja muzyka balansuje między organicznym brzmieniem a elektroniką, od gitar i perkusji po syntezatory i chórki, ale najważniejsze są w niej słowa: intymne, szczere i pełne obserwacji. 
+<br />
+To Alternatywny Pop, który wymaga uwagi. Na scenie szukam bliskości. 
+<br />
+Chcę, żeby słuchacz poczuł się częścią tej samej opowieści.</p>
             <h3>Kontakt</h3>
             <p>
               Manager: <a href="mailto:mgmt@aniaolczyk.com">mgmt@aniaolczyk.com</a>
@@ -19,6 +25,7 @@ const QRPage = () => {
             </p>
           </div>
           <div className="qr-video">
+            <h2>Nagranie Live:</h2>
             <iframe 
               src={`https://www.youtube.com/embed/0lCvE87M9Zg`}
               title={`Live Session - Ania Olczyk`}
@@ -28,7 +35,18 @@ const QRPage = () => {
             />
           </div>
         </div>
-        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3fXkjWQG9QA9Ddizcx6ykZ?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <div class="spotify-embed">
+          <iframe
+            src="https://open.spotify.com/embed/artist/1AfgDOc4Q0Z7LZpdQbU49y?utm_source=generator&theme=0"
+            style={{ borderRadius: '12px', width: '70%', height: '352px', marginTop: '25px' }}
+            frameBorder="0"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            allowFullScreen
+            loading="lazy"
+            title="Spotify Artist"
+          />
+
+        </div>
       </div>
     </section>
   );
